eb939f72d923abf5922cca37705f6eac907a78d9
Unknown <metsstijn@gmail.com>
2018-11-29 11:09:27 +0100

    changed UI

--- StackOverBros/StackOverBros/MainWindow.xaml.cs
+++ StackOverBros/StackOverBros/MainWindow.xaml.cs
@@ -20,9 +20,15 @@ namespace StackOverBros
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int x = 2;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
